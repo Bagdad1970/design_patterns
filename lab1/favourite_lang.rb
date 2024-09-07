47201b0ad8456ae3263744fd7067e8ad2181a5fa
Bogdan Belokobylskiy <BogdanBattle@mail.ru>
2024-09-07 21:28:13 +0300

    Added operations with os and ruby in favourite_lang

--- lab1/favourite_lang.rb
+++ lab1/favourite_lang.rb
@@ -16,3 +16,31 @@ case lower_lang
         puts "Скоро Ruby станет любимым"
 end
 
+username = ARGV[0] 
+puts "Привет, #{username}"
+
+print "Ваш любимый язык программирования: " 
+input_lang = STDIN.gets.chomp
+
+lower_lang = input_lang.downcase
+
+
+case lower_lang
+    when "ruby" then puts "Не подлизывайся"
+    when "c++" then puts "Крутой"
+    when "python" then puts "Зумер"
+    when "assembler" then puts "Бумер"
+    else
+        puts "Скоро Ruby станет любимым"
+end
+
+puts "Введите команду на языке Ruby:"
+ruby_instruction = STDIN.gets.chomp
+eval(ruby_instruction)
+
+print "Теперь команду ОС: "
+system_instruction = STDIN.gets.chomp
+
+puts "Вывод команды ОС:"
+system(system_instruction)
+
