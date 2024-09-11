768f824b2e05da378df1ec4d6171b68d58e29011
Bogdan Belokobylskiy <BogdanBattle@mail.ru>
2024-09-11 23:06:37 +0300

    Создан файл nums_and_loops.rb для задачи 4. И реализован первый метод по поиску максимального простого делителя числа

---
+++ introduction_to_ruby/nums_and_loops.rb
@@ -0,0 +1,50 @@
+def is_simple(num)
+  num = num.abs
+
+  if num == 0 then
+    return false
+  elsif num == 1 || num == 2 || num == 3 then
+    return true
+  end
+
+
+  for i in 2..(num/2+1) do
+    if num % i == 0 then
+      return false
+    end
+
+  end
+
+  return true
+
+
+end
+
+
+def max_simple_divider(number)
+  number = number.abs
+
+  if number == 0 then
+    return 
+  elsif number == 1 || number == 2 || number == 3 then
+    return number
+  end
+
+  if is_simple(number) == true then
+    return number
+  end
+
+  max_divider = 1
+
+  for potential_div in 2..(number/2+1) do
+    if number % potential_div == 0 then
+      if is_simple(potential_div) == true  && potential_div > max_divider then
+        max_divider = potential_div
+      end
+    end
+
+  end
+
+  return max_divider
+
+end
