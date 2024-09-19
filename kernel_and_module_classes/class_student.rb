3e4e452d736451f7375fc8f5c05e459cdcf78243
Bogdan Belokobylskiy <BogdanBattle@mail.ru>
2024-09-19 13:30:33 +0300

    Getter and Setters changed to attr_readers and attr_writer

--- kernel_and_module_classes/class_student.rb
+++ kernel_and_module_classes/class_student.rb
@@ -1,5 +1,8 @@
 class Student
-  
+ 
+  attr_reader :surname, :firstname, :lastname, :id, :phone_number, :telegram, :email, :git
+  attr_writer :surname, :firstname, :lastname, :id, :phone_number, :telegram, :email, :git
+
   def initialize(surname, firstname, lastname, id = nil, phone_number = nil, telegram = nil, email = nil, git = nil)
     @surname = surname
     @firstname = firstname
@@ -16,69 +19,4 @@ class Student
     "#{@id} ФИО: #{@surname} #{@firstname} #{@lastname} Данные для связи: #{@phone_number} #{@telegram} #{@email} #{git}"
   end
 
-  def surname
-    @surname
-  end
-
-  def firstname
-    @firstname
-  end
-
-  def lastname
-    @lastname
-  end
-
-  def id
-    @id
-  end
-
-  def phone_number
-    @phone_number
-  end
-
-  def telegram
-    @telegram
-  end
-
-  def email
-    @email
-  end
-
-  def git
-    @git
-  end
-
-
-  def surname=(val)
-    @surname = val
-  end
-
-  def firstname=(val)
-    @firstname = val
-  end
-
-  def lastname=(val)
-    @lastname = val
-  end
-
-  def id=(val)
-    @id = val
-  end
-
-  def phone_number=(val)
-    @phone_number = val
-  end
-
-  def telegram=(val)
-    @telegram = val
-  end
-
-  def email=(val)
-    @email = val
-  end
-
-  def git=(val)
-    @git = git
-  end
- 
 end
