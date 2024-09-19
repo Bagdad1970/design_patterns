6b0038cbce8672e769fa1992b73b67b69010b116
Bogdan Belokobylskiy <BogdanBattle@mail.ru>
2024-09-19 19:36:00 +0300

    Branch for class constructor with hash parametr

--- kernel_and_module_classes/class_student.rb
+++ kernel_and_module_classes/class_student.rb
@@ -2,15 +2,15 @@ class Student
  
   attr_accessor :surname, :firstname, :lastname, :id, :phone_number, :telegram, :email, :git
 
-  def initialize(surname, firstname, lastname, id: nil, phone_number: nil, telegram: nil, email: nil, git: nil)
+  def initialize(surname, firstname, lastname, optional_contacts)
     @surname = surname
     @firstname = firstname
     @lastname = lastname
-    @id = id
-    @phone_number = phone_number
-    @telegram = telegram
-    @email = email
-    @git = git
+    @id = optional_contacts[:id]
+    @phone_number = optional_contacts[:phone_number]
+    @telegram = optional_contacts[:telegram]
+    @email = optional_contacts[:email]
+    @git = optional_contacts[:git]
 
   end
 
