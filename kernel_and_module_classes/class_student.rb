19dc038b1659545e8442953946feec28bfc602ae
Bogdan Belokobylskiy <BogdanBattle@mail.ru>
2024-09-19 14:02:18 +0300

    Added the ability to enter optional fields in any order

--- kernel_and_module_classes/class_student.rb
+++ kernel_and_module_classes/class_student.rb
@@ -2,7 +2,7 @@ class Student
  
   attr_accessor :surname, :firstname, :lastname, :id, :phone_number, :telegram, :email, :git
 
-  def initialize(surname, firstname, lastname, id = nil, phone_number = nil, telegram = nil, email = nil, git = nil)
+  def initialize(surname, firstname, lastname, id: nil, phone_number: nil, telegram: nil, email: nil, git: nil)
     @surname = surname
     @firstname = firstname
     @lastname = lastname
--- kernel_and_module_classes/main.rb
+++ kernel_and_module_classes/main.rb
@@ -1,11 +1,11 @@
 require './class_student.rb'
 
 
-student1 = Student.new('Каупервуд', 'Фрэнк', 'Алджернон', 1, "+1 (455) 197-70-35", "@CowperwoodFinance", "CowperwoodF@gmail.com", "https://github.com/Cowperwood/")
+student1 = Student.new('Каупервуд', 'Фрэнк', 'Алджернон', id: 1, phone_number: "+1 (455) 197-70-35", telegram: "@CowperwoodFinance", email: "CowperwoodF@gmail.com", git: "https://github.com/Cowperwood/")
 
-student2 = Student.new('Мартин', 'Роберт', 'Сесил', 2, '+1 (576) 145-98-46', '@RMartin')
+student2 = Student.new('Мартин', 'Роберт', 'Сесил', id: 2, telegram: '@RMartin', phone_number: '+1 (576) 145-98-46')
 
-student3 = Student.new('Торвальдс', 'Линус', 'Бенедикт', 3, '+358 (567) 148-89-45', '@LinuxTorvalds', '', 'https://github.com/torvalds/linux')
+student3 = Student.new('Торвальдс', 'Линус', 'Бенедикт', id: 3, phone_number: '+358 (567) 148-89-45', telegram: '@LinuxTorvalds', git: 'https://github.com/torvalds/linux')
 
 puts student1
 puts student2
