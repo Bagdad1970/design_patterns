2db7f50102d15cd87308edee625d22cce4457faa
Bogdan Belokobylskiy <BogdanBattle@mail.ru>
2024-09-20 09:46:37 +0300

    Added phone number verification and exception handling to main.rb

--- kernel_and_module_classes/class_student.rb
+++ kernel_and_module_classes/class_student.rb
@@ -7,15 +7,28 @@ class Student
     @firstname = firstname
     @lastname = lastname
     @id = id
-    @phone_number = phone_number
+
+    if Student.is_phone_number_valid? (phone_number)
+      @phone_number = phone_number
+    else
+      raise ArgumentError.new("Неверный номер телефона для пользователя #{@id} #{@surname} #{@lastname} #{@firstname}")
+    end
+
     @telegram = telegram
     @email = email
     @git = git
 
   end
 
+  def Student.is_phone_number_valid?(checked_phone_number)
+    phone_number_reg = /^\+?\d\s?\(?\s*\d{3}\s*\)?\s?\d{3}\-{0,1}\d{2}\-{0,1}\d{2}\s*$/
+  
+    return checked_phone_number =~ phone_number_reg
+
+  end
+
   def to_s
-    "#{@id} ФИО: #{@surname} #{@firstname} #{@lastname} Данные для связи: #{@phone_number} #{@telegram} #{@email} #{git}"
+    "#{@id} ФИО: #{@surname} #{@firstname} #{@lastname}.\nДанные для связи:\nНомер телефона: #{@phone_number}\nТелеграм: #{@telegram}\nEmail: #{@email}\nGit: #{git}\n\n"
   end
 
 end
--- kernel_and_module_classes/main.rb
+++ kernel_and_module_classes/main.rb
@@ -1,12 +1,20 @@
 require './class_student.rb'
 
+def main
+  begin
+    student1 = Student.new('Каупервуд', 'Фрэнк', 'Алджернон', id: 1, phone_number: "+1 (455) 197-70-35", telegram: "@CowperwoodFinance", email: "CowperwoodF@gmail.com", git: "https://github.com/Cowperwood/")
+    puts student1
 
-student1 = Student.new('Каупервуд', 'Фрэнк', 'Алджернон', id: 1, phone_number: "+1 (455) 197-70-35", telegram: "@CowperwoodFinance", email: "CowperwoodF@gmail.com", git: "https://github.com/Cowperwood/")
+    student2 = Student.new('Мартин', 'Роберт', 'Сесил', id: 2, telegram: '@RMartin', phone_number: '+1 () 145-98-46')
+    puts student2
 
-student2 = Student.new('Мартин', 'Роберт', 'Сесил', id: 2, telegram: '@RMartin', phone_number: '+1 (576) 145-98-46')
+    student3 = Student.new('Торвальдс', 'Линус', 'Бенедикт', id: 3, phone_number: '+358 (123) 148-89-4', telegram: '@LinuxTorvalds', git: 'https://github.com/torvalds/linux')
 
-student3 = Student.new('Торвальдс', 'Линус', 'Бенедикт', id: 3, phone_number: '+358 (567) 148-89-45', telegram: '@LinuxTorvalds', git: 'https://github.com/torvalds/linux')
+    puts student3
+  rescue => err
+    puts err
+  end
 
-puts student1
-puts student2
-puts student3
+end
+
+main()
