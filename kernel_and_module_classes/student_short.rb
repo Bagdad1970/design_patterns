1c4136d3988f74f6ec023df642cbc359ef869119
Bogdan Belokobylskiy <BogdanBattle@mail.ru>
2024-10-08 22:10:58 +0300

    LR2.T2.5.2 Refactoring class Student and Student_Short
    All common methods and fields for Student and Student_Short moved to
    Person

--- kernel_and_module_classes/student_short.rb
+++ kernel_and_module_classes/student_short.rb
@@ -1,27 +1,32 @@
-class Student_Short
+require_relative 'person.rb'
 
-  attr_reader :id, :name, :git, :contact
-  attr_accessor :id, :name, :git, :contact
+class Student_Short < Person
   
   def initialize(id:, name:, git:, contact:)
-    self.id = id
-    self.name = name
-    self.git = git
-    self.contact = contact
+    initials = name.split[1].split('.').map(&:capitalize)
+    surname, firstname, lastname = name.split[0], initials[0], initials[1]
+
+    if id.nil?
+      raise ArgumentError.new("Отсутствует id студента: #{surname} #{firstname}.#{lastname}.")
+    end
+
+    super(surname: surname, firstname: firstname, lastname: lastname, id: id, git: git, contact: contact)
+
   end
 
-  def Student_Short.create_from_string(id, data)
+  private_class_method :new
+
+  def Student_Short.create_from_string(id:, data:)
     student_short_init = {}
 
     params = Student_Short.parse_string_params(data)
 
     student_short_init[:id] = id
-    student_short_init[:name] = params[0] + ' ' + params[1]
+    student_short_init[:name] = params[0..1].join(' ')
     student_short_init[:git] = params[2]
     student_short_init[:contact] = params[3..].join('')
-
     self.new(**student_short_init)
-      
+
   end
 
   def Student_Short.create_from_student(student_obj)
@@ -30,7 +35,7 @@ class Student_Short
     student_short_init[:id] = student_obj.id
     student_short_init[:name] = student_obj.get_name
     student_short_init[:git] = student_obj.git
-    student_short_init[:contact] = student_obj.get_contact
+    student_short_init[:contact] = student_obj.contact
 
     self.new(**student_short_init)
 
@@ -41,8 +46,7 @@ class Student_Short
   end
 
   def to_s
-    "#{@id} #{@name} #{@git} #{@contact}"
+    "#{@id} #{self.get_name} #{@git} #{@contact}"
   end
 
 end
-
