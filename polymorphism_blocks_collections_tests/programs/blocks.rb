5362b9763c7a0e06125e4f088d9ca9ef666e3d10
Bogdan Belokobylskiy <BogdanBattle@mail.ru>
2024-10-26 00:51:56 +0300

    LR4.T1.3 Realize method circular_shift_to_left

---
+++ polymorphism_blocks_collections_tests/programs/blocks.rb
@@ -0,0 +1,20 @@
+def passing_array (array, index)
+  if index <= 0 || index >= array.size
+    raise ArgumentError.new('Неверный индекс')
+  end
+
+  yield(array, array[index])
+end
+
+def is_elem_global_max? (array, index)
+  passing_array(array, index) do |array, element|
+    return element == array.max
+  end
+end
+
+def circular_shift_to_left (array)
+  first_elem = array.shift
+
+  return array.append(first_elem)
+end
+
