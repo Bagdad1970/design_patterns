9683a02540c735fda9d2b1198be6e0d9ce4aee40
Bogdan Belokobylskiy <BogdanBattle@mail.ru>
2024-11-01 23:07:40 +0300

    LR3.T3.4 Mix Comparable to Tag

--- blocks_trees/html_tree/main.rb
+++ blocks_trees/html_tree/main.rb
@@ -1,10 +1,20 @@
-require './n_tree.rb'
+require './html_tree_dfs.rb'
+require './html_tree_bfs.rb'
 
 def main
   begin
     html_tree = HTML_Tree.create_from_file('./index.html')
 
     p html_tree
+
+    html_tree_dfs = HTML_Tree_DFS.create_from_file('./index.html')
+    html_tree_dfs.each{|node| puts "#{node}\n"}
+    puts html_tree_dfs.count
+
+    html_tree_bfs = HTML_Tree_BFS.create_from_file('./index.html')
+    html_tree_bfs.each{|node| puts "#{node}\n"}
+
+    puts html_tree_dfs.count{|tag| tag.features.nil?}
 
   rescue => error
     puts error
--- blocks_trees/html_tree/tag.rb
+++ blocks_trees/html_tree/tag.rb
@@ -1,8 +1,10 @@
 class Tag
 
+  include Comparable
+
   attr_accessor :children, :parent, :name, :features, :info
   
-  def initialize(name: , features: nil, info: nil, children: [], parent: nil)
+  def initialize(name: , features: nil, info: nil, children: nil, parent: nil)
     self.name = name
     self.features = features
     self.info = info
@@ -10,7 +12,27 @@ class Tag
     self.parent = parent
   end
 
-  def has_tag_features?  # bolean
+  def <=>(other)
+    self.name <=> other.name
+  end
+
+  def each_dfs(&block)
+    yield self
+    children.each{|node| node.each_dfs(&block)} if children
+  end
+
+  def each_bfs(&block)
+    queue = [self]
+    while !(queue.empty?)
+      current_node = queue.shift
+      yield current_node
+      current_node.children.each do |child|
+        queue.append(child)
+      end
+    end
+  end
+
+  def has_tag_features?  # boolean
     !(self.features.nil?)
   end
 
@@ -23,7 +45,7 @@ class Tag
   end
 
   def to_s
-    "Тег: #{@name} #{@features}#{@info}\nРодитель: #{@parent} Дети: #{@children}"
+    "Тег: #{@name} #{@features}#{@info}\n"
   end
 
 end
