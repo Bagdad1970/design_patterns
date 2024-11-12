0318049cf1c95a2038029596668a44ca3a5d953c
Bogdan Belokobylskiy <BogdanBattle@mail.ru>
2024-11-12 17:37:26 +0300

    LR4.T2.4 Create base class Student_List
    Classes Student_List_TXT, Student_List_JSON, Student_List_YAML inherits
    from Student_List. Superclass has all common methods and features.
    Derived classes has only methods only for working with files of their
    format

--- simple_patterns/model_classes/main.rb
+++ simple_patterns/model_classes/main.rb
@@ -6,6 +6,37 @@ require './student_list_txt.rb'
 require './student_list_json.rb'
 require './student_list_yaml.rb'
 
+def execute_data_list_data_table
+  data_table = Data_Table.new([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+
+  puts data_table
+
+  data_list = Data_List.new(['eifhei', 'hello', 'hi', 'bruh'])
+
+  data_list.select(1)
+  data_list.select(2)
+
+  puts data_list.get_selected
+
+  puts data_list
+    
+  student_short1 = Student_Short.create_from_string(id: 1, data: 'Каупервуд Ф.А. github.com/Cowperwood/ @CowperwoodFinance')
+
+  student_short2 = Student_Short.create_from_string(id: 2, data: 'Мартин Р.С. github.com/robert_martin/ +1(123)554-78-93')
+
+  student_short3 = Student_Short.create_from_string(id: 3, data: 'Белокобыльский Б.В. github.com/bagdad1970/ +7(905)505-57-54')
+
+  student_short4 = Student_Short.create_from_string(id: 4, data: 'Жиллиман Р.Г. github.com/space_marine/ RoboutEmperium@gmail.com')
+
+  data_list_student_short = Data_List_Student_Short.new([student_short1, student_short2])
+  puts data_list_student_short.get_data_table_with_values_by_names
+
+  data_list_student_short.sorted_array = [student_short3, student_short4]
+  puts data_list_student_short.get_data_table_with_values_by_names
+
+end
+
+
 def execute_student_list_txt
   student1 = Student.new(surname: 'Каупервуд', firstname: 'Фрэнк', lastname: 'Алджернон', birthdate: '2000/12/12', phone_number: '+7 (905) 404-57-54', telegram: "@CowperwoodFinance", email: "CowperwoodF@gmail.com", git: "github.com/Cowperwood/")
 
@@ -17,13 +48,12 @@ def execute_student_list_txt
     
   student_array = [student1, student2, student3, student4]
 
-  student_list_txt = Student_List_TXT.new(filepath: './output_student_txt')
-  student_list_txt.read_from_txt()
+  student_list_txt = Student_List_TXT.new(filepath: './output_txt')
+  
+  student_list_txt.write_to_file(student_array)
 
-  puts student_list_txt.get_k_n_student_short_list(page: 1)
+  student_list_txt.read_from_file()
 
-  student_list_txt.write_to_txt(student_array)
-    
   puts student_list_txt.get_k_n_student_short_list(page: 1)
 
   puts student_list_txt.sort_by_name
@@ -36,7 +66,6 @@ end
 
 def execute_student_list_json
 
-=begin
   student1 = Student.new(surname: 'Каупервуд', firstname: 'Фрэнк', lastname: 'Алджернон', birthdate: '2000/12/12', phone_number: '+7 (905) 404-57-54', telegram: "@CowperwoodFinance", email: "CowperwoodF@gmail.com", git: "github.com/Cowperwood/")
 
   student2 = Student.new(surname: 'Каупервуд', firstname: 'Фрэнк', lastname: 'Алджернон', birthdate: '1845/03/06')
@@ -46,9 +75,10 @@ def execute_student_list_json
   student4 = Student.new(surname: 'Атрейдес', firstname: 'Пол', lastname: 'Летович', birthdate: '1980/01/1')
 
   student_array = [student1, student2, student3, student4]
-=end
 
   student_list_json = Student_List_JSON.new(filepath: './output_json.json')
+
+  student_list_json.write_to_file(student_array)
 
   student_list_json.read_from_file()
   puts student_list_json.get_student_by_id(2)
@@ -57,13 +87,12 @@ def execute_student_list_json
 
   puts student_list_json.sort_by_name
 
-  #student_list_json.write_to_file(student_array, writing_mode = 'w')
+  
     
 
 end
 
 def execute_student_list_yaml
-=begin
   student1 = Student.new(surname: 'Каупервуд', firstname: 'Фрэнк', lastname: 'Алджернон', birthdate: '2000/12/12', phone_number: '+7 (905) 404-57-54', telegram: "@CowperwoodFinance", email: "CowperwoodF@gmail.com", git: "github.com/Cowperwood/")
 
   student2 = Student.new(surname: 'Каупервуд', firstname: 'Фрэнк', lastname: 'Алджернон', birthdate: '1845/03/06')
@@ -73,51 +102,22 @@ def execute_student_list_yaml
   student4 = Student.new(surname: 'Атрейдес', firstname: 'Пол', lastname: 'Летович', birthdate: '1980/01/1')
   
   student_array = [student1, student2, student3, student4]
-=end
 
   student_list_yaml = Student_List_YAML.new(filepath: './output_yaml.yaml')
 
+  student_list_yaml.write_to_file(student_array)
   puts student_list_yaml.read_from_file
-
-  #student_list_yaml.write_to_file(student_array)
-
 
 end
 
 
 def main
   begin
-
-    data_table = Data_Table.new([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
-
-    puts data_table
-
-    data_list = Data_List.new(['eifhei', 'hello', 'hi', 'bruh'])
-
-    data_list.select(1)
-    data_list.select(2)
-
-    puts data_list.get_selected
-
-    puts data_list
+    execute_data_list_data_table
     
-    student_short1 = Student_Short.create_from_string(id: 1, data: 'Каупервуд Ф.А. github.com/Cowperwood/ @CowperwoodFinance')
+    execute_student_list_txt
 
-    student_short2 = Student_Short.create_from_string(id: 2, data: 'Мартин Р.С. github.com/robert_martin/ +1(123)554-78-93')
-
-    student_short3 = Student_Short.create_from_string(id: 3, data: 'Белокобыльский Б.В. github.com/bagdad1970/ +7(905)505-57-54')
-
-    student_short4 = Student_Short.create_from_string(id: 4, data: 'Жиллиман Р.Г. github.com/space_marine/ RoboutEmperium@gmail.com')
-
-    data_list_student_short = Data_List_Student_Short.new([student_short1, student_short2])
-    puts data_list_student_short.get_data_table_with_values_by_names
-
-    data_list_student_short.sorted_array = [student_short3, student_short4]
-    puts data_list_student_short.get_data_table_with_values_by_names
-
-    #execute_student_list_txt
-
-    #execute_student_list_json
+    execute_student_list_json
 
     execute_student_list_yaml
     
--- simple_patterns/model_classes/student_list_yaml.rb
+++ simple_patterns/model_classes/student_list_yaml.rb
@@ -1,28 +1,12 @@
-require './student.rb'
-require './student_short.rb'
-require './data_list_student_short.rb'
+require_relative 'student_list'
+
 require 'yaml'
-require 'date'
 
-class Student_List_YAML
+class Student_List_YAML < Student_List
 
-  attr_accessor :filepath
-  attr_reader :student_array
-
-  def initialize(filepath: , student_array: nil)
-    self.filepath = filepath
-    self.student_array = student_array
+  def initialize(filepath:, student_array: nil)
+    super(filepath: filepath, student_array: student_array)
   end
-
-  def student_array=(student_array)
-    if student_array.nil? or student_array.is_a? (Array)
-      @student_array = student_array
-    else
-      raise TypeError.new('Неверный тип входных данных')
-    end
-  end
-
-  private :filepath=, :student_array=
 
   def read_from_file
     yaml_data = YAML.safe_load(File.read(self.filepath), permitted_classes: [Date, Symbol])
@@ -37,51 +21,6 @@ class Student_List_YAML
     File.open(self.filepath, writing_mode) do |file|
       file.write(YAML.dump(hash_student_array)) 
     end
-  end
-
-  def get_student_by_id(required_id)
-    return self.student_array.find {|student| student.id == required_id}
-  end
-
-  def get_k_n_student_short_list(page:, amount_rows: 20, data_list: nil)
-    if page <= 0
-      raise ArgumentError.new('Недопустимый номер страницы')
-    elsif amount_rows < 0
-      raise ArgumentError.new('Недопустимое количество записей')
-    end
-
-    student_short_array = self.student_array.each_with_object([]) {|student, array| array.append(Student_Short.create_from_student(student))}
-
-    start_index = (page-1) * amount_rows
-    end_index = [start_index + amount_rows - 1, student_short_array.size - 1].min
-
-    data_list = Data_List_Student_Short.new(student_short_array[start_index..end_index])
-
-    return data_list 
-  end
-
-  def sort_by_name
-    return self.student_array.sort_by {|student| student.get_name}
-  end
-
-  def add_student(student)
-    self.student_array.append(student)
-  end
-
-  def replace_student_by_id(required_id, new_student)
-    index = self.student_array.find_index {|student| student.id == required_id}
-
-    if index
-      self.student_array[index] = new_student
-    end
-  end
-
-  def delete_student_by_id(required_id)
-    self.student_array.delete_if {|student| student.id == required_id}
-  end
-
-  def get_student_short_count
-    self.student_array.size
   end
 
 end
