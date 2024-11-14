a24d06e277dce83045a2b09535fdc293cf806792
Bogdan Belokobylskiy <BogdanBattle@mail.ru>
2024-11-14 19:22:10 +0300

    LR4.T3.5 Execute SELECT

--- simple_patterns/model_classes/database/table_students.rb
+++ simple_patterns/model_classes/database/table_students.rb
@@ -5,19 +5,31 @@ def connect_to_db(host: , username:, password: , database:)
   return client
 end
 
-def create_students(client, create_filepath, insert_filepath)
+def create_students(client, insert_filepath)
   create_command = File.read(create_filepath)
-  insert_command = File.read(insert_filepath)
 
   client.query(create_command)
+end
+
+def insert_into_students(client, insert_filepath)
+  insert_command = File.read(insert_filepath)
 
   client.query(insert_command)
+end
+
+def select_from_students(client)
+  result = client.query('SELECT * FROM STUDENTS;')
+  result.each {|row| puts row}
 end
 
 def main()
   client = connect_to_db(host: 'localhost', username: 'bagdad', password: '123', database: 'mysql')
 
-  create_students(client, './structure/students.txt', './data/students.txt')
+  create_students(client, './structure/students.txt')
+
+  insert_into_students(client, './data/students.txt')
+
+  select_from_students(client)
 end
 
 main
