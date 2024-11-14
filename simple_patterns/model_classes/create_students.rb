12becceb1739b805622089a7da397c1085e5437b
Bogdan Belokobylskiy <BogdanBattle@mail.ru>
2024-11-14 16:46:57 +0300

    LR4.T3.1 Add method for creating table Students

--- simple_patterns/model_classes/create_students.rb
+++ simple_patterns/model_classes/create_students.rb
@@ -5,8 +5,28 @@ def connect_to_db(host: , username:, password: , database:)
   return client
 end
 
-def main
+def create_students(client)
+  client.query("CREATE TABLE IF NOT EXISTS STUDENTS (
+                  STUDENT_ID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+                  SURNAME VARCHAR(40) NOT NULL,
+                  FIRSTNAME VARCHAR(40) NOT NULL,
+                  LASTNAME VARCHAR(100) NOT NULL,
+                  BIRTHDATE DATE NOT NULL,
+                  PHONE_NUMBER VARCHAR(20) NULL,
+                  TELEGRAM VARCHAR(35) NULL,
+                  EMAIL VARCHAR(40) NULL,
+                  GIT VARCHAR(40) NULL
+                );"
+              )
+
+end
+
+def main()
+
   client = connect_to_db(host: 'localhost', username: 'bagdad', password: '123', database: 'mysql')
+
+  create_students(client)
+
 end
 
 main
