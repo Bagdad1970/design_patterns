bb4f063431ee837ae7bb44b139185e10fb1b3086
Bogdan Belokobylskiy <BogdanBattle@mail.ru>
2025-01-11 16:36:48 +0300

    LR4.T5.3 Add method filter_names in FilterDecorator
    filter_names returns array with all filter names of filter object

--- student_classes/filter/base_filter.rb
+++ student_classes/filter/base_filter.rb
@@ -1,9 +1,8 @@
 require_relative 'ifilter.rb'
 
 class BaseFilter < IFilter
-
+  
   def apply(students)
     return students
   end
-
 end
--- student_classes/filter/filter_decorator.rb
+++ student_classes/filter/filter_decorator.rb
@@ -10,4 +10,12 @@ class FilterDecorator < IFilter
     @wrappee.apply(students)
   end
 
+  def filter_names
+    if @wrappee.respond_to?(:filter_names)
+      @wrappee.filter_names << self.class.name
+    else
+      return [self.class.name]
+    end
+  end
+
 end
